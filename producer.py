1d3b39c8a532c4608b17ce508a6ac6a20bcb90a3
Vadim.Shpakovsky <vadim.shpakovsky@shell.com>
2023-03-31 15:00:20 +0200

    Cli interface for Producer
    No implementation, just skeleton of CLI interface:
    * producer.py produce --list-impls
    * producer.py produce --impl-name python-kafka

--- producer.py
+++ producer.py
@@ -1,15 +1,19 @@
 import asyncio
+import importlib
 import logging
 import time
 from typing import AsyncGenerator, Generator
 
+import click
+import yaml
 from aiokafka import AIOKafkaProducer
 from kafka import KafkaAdminClient, KafkaProducer
 from kafka.admin import NewTopic
 
-from utils.logging_utils import setup_logger
+from utils import setup_logger
 
 setup_logger()
+
 
 # Kafka server
 KAFKA_SERVER_URL = "localhost:9092"
@@ -74,11 +78,11 @@ class AsyncProducer:
             counter += 1
 
 
-class SyncProducer:
-    producer: KafkaProducer
+class PythonKafkaProducer:
+    kafka_producer: KafkaProducer
 
-    def __init__(self):
-        self.producer = KafkaProducer(bootstrap_servers=KAFKA_SERVER_URL)
+    def __init__(self, config: dict):
+        self.kafka_producer = KafkaProducer(bootstrap_servers=KAFKA_SERVER_URL)
         # setup_kafka()
 
     def run(self):
@@ -87,11 +91,11 @@ class SyncProducer:
         try:
             for message in self._data_gen():
                 logging.info(f"Put message to topic: '{message}'")
-                self.producer.send(
+                self.kafka_producer.send(
                     KAFKA_TOPIC, message.encode("UTF-8"), key="123".encode()
                 )
         finally:
-            self.producer.close()
+            self.kafka_producer.close()
 
         logging.info("Finish producer")
 
@@ -110,13 +114,64 @@ async def run_async_producer():
 
 
 def run_sync_producer():
-    producer = SyncProducer()
+    producer = PythonKafkaProducer(None)
     producer.run()
 
 
+@click.group()
+def cli():
+    pass
+
+
+@click.group()
+@click.option("--config", "-c", type=click.Path(exists=True), default="config.yaml")
+@click.pass_context
+def cli(ctx, config):
+    with open(config) as f:
+        ctx.obj = {"config": yaml.safe_load(f)}
+
+
+@cli.command()
+@click.option("--impl-name", required=True, help="Name of the implementation to use")
+@click.pass_context
+def produce(ctx, impl_name):
+    config = ctx.obj["config"]
+
+    # Parse implementation class name
+    try:
+        impl_class_name = config["producer"]["implementations"][impl_name]
+    except KeyError:
+        raise click.BadParameter(
+            param_hint="--impl-name",
+            message="supported values are provided with list_impls command",
+        )
+
+    # Dynamically import the implementation class
+    try:
+        module = importlib.import_module(__name__)
+        impl_class = getattr(module, impl_class_name)
+    except (ImportError, AttributeError):
+        click.echo(f"ERROR: Could not find implementation class: {impl_class_name}")
+        return
+
+    # run producer
+    producer = impl_class(config)
+    producer.run()
+
+
+@cli.command()
+@click.pass_context
+def list_impls(ctx):
+    impls = ctx.obj["config"]["producer"]["implementations"]
+    for name in impls:
+        click.echo(name)
+
+
 if __name__ == "__main__":
+    cli()
+
     # run async producer
     # asyncio.run(run_async_producer())
 
     # run sync producer
-    run_sync_producer()
+    # run_sync_producer()
